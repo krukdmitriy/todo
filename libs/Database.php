0a760be2c037fd08ffc3d528f14bc59091ab1137
NoRAV <krukdmitriyv@gmail.com>
2019-11-03 13:34:53 +0200

    fix db connect

--- libs/Database.php
+++ libs/Database.php
@@ -4,7 +4,7 @@ class Database extends PDO
 {
     function __construct()
     {
-        parent::__construct('mysql:host='.DB_HOST.';dbname='.DB_HOST.';', DB_USER, DB_PASSWORD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        parent::__construct('mysql:host='.DB_HOST.';dbname='.DB_NAME.';', DB_USER, DB_PASSWORD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     }
 
 }
